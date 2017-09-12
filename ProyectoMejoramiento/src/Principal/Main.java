7e54ced0059902f6164b290c9e0294fc50be819c
Juandi Vallejo <juadival@espol.edu.ec>
2017-09-11 20:06:32 -0500

    Cambios en Main

--- ProyectoMejoramiento/src/Principal/Main.java
+++ ProyectoMejoramiento/src/Principal/Main.java
@@ -7,6 +7,7 @@ package Principal;
 
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,13 +18,36 @@ import javafx.stage.Stage;
  *
  * @author Juandi
  */
-public class Main {
+public class Main extends Application{
     
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // This loads the FXML files for the GUI
+        Parent root = FXMLLoader.load(getClass().getResource("quizWindow.fxml"));
+        
+        // Adds the icon        
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+
+        // Loads the CSS for the FXML
+        root.getStylesheets().add("application/lightTheme.css");
+        primaryStage.setResizable(false);
+        
+  	// Did this to preserve "nice-lookingness" of the GUI
+        primaryStage.setTitle("Gunvir's Quiz Game!");
+        primaryStage.setOnCloseRequest((event -> {
+  	// Terminates the scheduled TimerTask to display next question
+            Platform.exit();
+            System.exit(0);
+        }));
+        
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);        
     }
     
 }
